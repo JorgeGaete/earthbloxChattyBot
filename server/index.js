9cdeea9d10fed3bc237bd6c5b72e1e538f4ff4b5
JorgeGaete <j.a.gaete@ed.ac.uk>
2024-05-27 16:32:37 +0100

    create assistant API backend connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
+const OpenAI = require("openai")
+
 app.use(express.json())
 app.use(cors())
+
 
 const port = 5000;
 app.listen(port, () => console.log(`Earthblox app listening on port ${port}!`));
 
-app.post('/createAssistant', (req, res) => {
+app.post('/createAssistant', async (req, res) => {
     const name = req.body.name;
     const instruction = req.body.instruction;
     const model = req.body.model;
@@ -20,6 +22,8 @@ app.post('/createAssistant', (req, res) => {
         "description": description,
         "response_format": { "type": "json_object" }
     }
-    console.log(create_req_json)
-    res.json(create_req_json)
+    const openai = new OpenAI();
+    const assistant = await openai.beta.assistants.create(create_req_json)
+    console.log(assistant)
+    res.json(assistant)
 });
